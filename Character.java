455025529c96089c3f6e25680d6bdf15bdd1484b
shahdabdellatif <shahd.abdellatif5@gmail.com>
2025-04-06 13:31:06 -0500

    character class

--- Character.java
+++ Character.java
@@ -1,3 +1,75 @@
-public class Character {
+/**
+ * @author: Shahd Abdellatif & Inaya
+ * @since : 04/05/2025
+ *
+ */
+public class Character
+{
+    private  String name;
+    private int health;
+    private  int strength;
+    private  int speed;
 
+    private static final int  BASE_ATTACK=10;
+    private static final int BASE_HEAL=5;
+    private static final int MAX_HEALTH=50;
+
+    public Character(String name,int strength,int speed)
+    {
+        this.name=name;
+        this.strength=strength;
+        this.speed=speed;
+        this.health=MAX_HEALTH;
+
+    }
+    public  int attack(Character opponent,int randNum)
+    {
+        int damage = BASE_ATTACK+strength;
+        if(randNum==1)
+            damage+=5;
+        opponent.subtractDamage(damage);
+        return damage;
+    }
+    public void subtractDamage(int x)
+    {
+        health-=x;
+
+    }
+
+    public int heal()
+    {
+        int heal=BASE_HEAL+(2*speed);
+        int healthLost=MAX_HEALTH-health;
+
+        if(heal>healthLost)
+            heal=healthLost;
+        health+=heal;
+        return heal;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getStrength()
+    {
+        return strength;
+
+    }
+
+    public int getSpeed()
+    {
+        return speed;
+    }
+
+    public int getHealth()
+    {
+        return health;
+    }
+
+    public String toString()
+    {
+        return name+"[Strength: "+strength+", Speed: "+speed+", Health: "+ "]";
+    }
 }
